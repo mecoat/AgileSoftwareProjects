f9f2fdf552f0c5692c065dbe44b05237fc612137
mecoat <66117150+mecoat@users.noreply.github.com>
2022-08-02 22:49:43 +0100

    fixed faulty registration at end of day

--- JS/Script.js
+++ JS/Script.js
@@ -10,98 +10,72 @@ function drawTimetable(){
     //create an empty placeholder for content
     var timetableContent = "";
 
-    //Get weeks from document    
-    var weeks = document.getElementsByName("weeks");
     //call function to get weeks in timetable
-    var noWeeks = getActive (weeks);
+    var noWeeks = getActiveElement("weeks")
     //add content to the placeholder to add to the DOM
     timetableContent += createHeaderRow(noWeeks);
 
-    //Get periods from document    
-    var periods = document.getElementsByName("periods");
-    //call function to get weeks in timetable
-    var noPeriods = getActive (periods);
+    //call function to get periods in timetable
+    var noPeriods = getActiveElement ("periods");
     
-    //get lunch from DOM
-    var lunch = document.getElementsByName("lunch");
     //get lunchtime with function
-    var lunchTime = getActive(lunch);
+    var lunchTime = getActiveElement("lunch");
     
-    //get breaks from DOM
-    var breaks = document.getElementsByName("breaks");
     //get number of breaks with function
-    var noBreaks = getActive(breaks); 
+    var noBreaks = getActiveElement("breaks"); 
     
     //if there's 1 or more breaks
     if (noBreaks > 0){
-        //get first break from DOM
-        var break1 = document.getElementsByName("break1");
         //get first breaktime with function
-        var break1Time = getActive(break1); 
+        var break1Time = getActiveElement("break1"); 
     }
     //if there's 2 breaks
     if (noBreaks > 1){
-        //get second break from DOM
-        var break2 = document.getElementsByName("break2");
         //get second breaktime with function
-        var break2Time = getActive(break2); 
+        var break2Time = getActiveElement("break2"); 
     }
 
-    //get registration from DOM
-    var regPeriods = document.getElementsByName("regPeriods");
-    //get number of breaks with function
-    var noRegPeriods = getActive(regPeriods); 
+    //get number of registration periods with function
+    var noRegPeriods = getActiveElement("regPeriods"); 
 
     //if there's 1 or more reg periods
     if (noRegPeriods > 0){
-        //get first break from DOM
-        var regPeriods1 = document.getElementsByName("regPeriods1");
-        //get first breaktime with function
-        var reg1Time = getActive(regPeriods1); 
+        //get first reg period with function
+        var reg1Time = getActiveElement("regPeriods1"); 
     }
     //if there's 2 reg periods
     if (noRegPeriods > 1){
-        //get second break from DOM
-        var regPeriods2 = document.getElementsByName("regPeriods2");
-        //get second breaktime with function
-        var reg2Time = getActive(regPeriods2); 
+        //get second reg period with function
+        var reg2Time = getActiveElement("regPeriods2"); 
     }
 
     var reg1LunchTime = 0;
     //check if reg1 is adjacent to lunch, find out if before/After
     if (reg1Time && reg1Time == lunchTime){
-        //get registration from DOM
-        var regLunch1 = document.getElementsByName("regLunch1");
-        //get number of breaks with function
-        reg1LunchTime = getActive(regLunch1); 
+        //get value with function
+        reg1LunchTime = getActiveElement("regLunch1"); 
     }
 
     var reg1BreakTime = 0;
     //check if reg1 is adjacent to lunch, find out if before/after
     if (reg1Time && reg1Time == lunchTime){
-        //get registration from DOM
-        var regBreak1 = document.getElementsByName("regBreak1");
-        //get number of breaks with function
-        reg1BreakTime = getActive(regBreak1); 
+        //get value with function
+        reg1BreakTime = getActiveElement("regBreak1"); 
     }
 
     var reg2LunchTime = 0;
     //check if reg2 is adjacent to lunch, find out if before/After
     if (reg2Time && reg2Time == lunchTime){
-        //get registration from DOM
-        var regLunch2 = document.getElementsByName("regLunch2");
-        //get number of breaks with function
-        reg2LunchTime = getActive(regLunch2); 
+        //get value with function
+        reg2LunchTime = getActiveElement("regLunch2"); 
     }
 
 
     var reg2BreakTime = 0;
     //check if reg2 is adjacent to lunch, find out if before/after
     if (reg2Time && reg1Time == lunchTime){
-        //get registration from DOM
-        var regBreak2 = document.getElementsByName("regBreak2");
-        //get number of breaks with function
-        reg2BreakTime = getActive(regBreak2); 
+        //get value with function
+        reg2BreakTime = getActiveElement("regBreak2"); 
     }
 
     //add content to the placeholder to add to the DOM
@@ -120,7 +94,7 @@ function drawTimetable(){
     } 
     
     if (reg1Time == "Last" || reg2Time == "Last"){
-        timetableContent += createTimetableRow("Registration");
+        timetableContent += createTimetableRow(noWeeks, "Registration");
     }
 
     //add the content to the DOM
