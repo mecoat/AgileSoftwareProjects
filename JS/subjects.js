a127ae837d62695eb0cbaaabaf5c44da6baa5c97
mecoat <66117150+mecoat@users.noreply.github.com>
2022-08-09 16:44:23 +0100

    separated tt from script.js, added subj base files

---
+++ JS/subjects.js
@@ -0,0 +1,10 @@
+function subStart(){
+    //draw the timetable to screen
+    // drawTimetable(); 
+    //add the event listeners for the timetable choice buttns
+    // addTTEventListeners (); 
+    //hide optional values
+    // hideOnTTPage();
+    //add event listener to user input
+    // addUIEventListeners ();
+}
