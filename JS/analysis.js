76593d60c9ad238af4a1479e6b7d133ce85c958b
mecoat <66117150+mecoat@users.noreply.github.com>
2022-08-30 00:10:11 +0100

    user error messages amended

---
+++ JS/analysis.js
@@ -0,0 +1,67 @@
+//global variables for processing
+//table headers
+var subjectTableHeaders = ["Subject Code", "Subject", "Main Teacher Periods"];
+var swapsTableHeaders = ["Teacher Code", "Teacher", "Periods", "Subject Code", "Subject"]
+//arrays for data
+var subjectPeriods = [];
+var teacherSubjectPeriods = [];
+var totalledSubjectPeriods = [];
+var teacherSwaps = [];
+
+
+function analysisStart(){
+
+    // //add the event listener for the subjects Load button
+    // addSubLoadEventListener (); 
+    // //add the event listeners for the subject style buttons
+    // addSubStyleEventListener (); 
+
+    // //add the event listener for the Set Block Button
+    // addBlockEventListener ();
+
+    // //add the event listener for the Set Band Button
+    // addBandEventListener ();
+
+    // //add the event listener for the Set Class Button
+    // addClassEventListener ();
+
+    // //draw the band list to screen
+    // drawBand (); 
+
+    // //add the event listener for the Class Delete button (within Band)
+    // addClassDeleteEventListener ();
+    // //add the event listener for the Band table
+    // addBandTableEventListener ();
+
+    // //add the event listener for the Save Band button
+    // addSaveBandEventListener (); 
+
+    // //draw the block list to screen
+    // drawBlock (); 
+
+    // //add the event listener for the Band Delete button (within Block)
+    // addBandDeleteEventListener ();
+    // //add the event listener for the Block table
+    // addBlockTableEventListener ();
+
+    // //add the event listener for the Save Block button
+    // addSaveBlockEventListener (); 
+
+    // //draw the total list to screen
+    // drawTotal(); 
+
+    // //add the event listener for the total table
+    // addTotalTableEventListener (); 
+    // //add the event listener for the Band Delete button (within Total)
+    // addBlockDeleteEventListener ();
+    
+
+    // //add the event listener for the Save button
+    // addSaveEventListener ();
+    // //add the event listener for the Template button
+    // addTemplateEventListener ();
+
+    // //add the event listener for the Load button
+    // addLoadEventListener (); 
+
+}
